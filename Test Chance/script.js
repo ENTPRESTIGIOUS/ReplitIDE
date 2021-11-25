5e1c2ce4fa85033784483333de243ca392e08cc5
ELITE NOTORIOUS THE PRESTIGIOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2021-11-25 08:39:45 +0000

    commit

---
+++ Test Chance/script.js
@@ -0,0 +1,7 @@
+let percentage = 75;
+// let round = Math.round(value);
+let decimal = Math.random() * (percentage / 100);
+let shouldIFill = Math.round(Math.random() * (percentage / 100));
+
+console.log(shouldIFill);
+// console.log(decimal);
